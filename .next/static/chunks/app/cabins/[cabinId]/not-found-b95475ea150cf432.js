989bc5cf8331ad46f4a95a1690230b0c9791a794
naveedahmadcodes <naveedahmad.codes@gmail.com>
2026-07-05 12:26:43 +0500

    Remove build artifacts from repo

--- .next/static/chunks/app/cabins/[cabinId]/not-found-b95475ea150cf432.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[810,160,642],{9083:function(n,e,u){Promise.resolve().then(u.t.bind(u,5250,23))}},function(n){n.O(0,[250,971,69,744],function(){return n(n.s=9083)}),_N_E=n.O()}]);
