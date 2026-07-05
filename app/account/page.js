9b8a04f18b5dd5e39b4b12e8c5bc142663e8b598
naveedahmadcodes <naveedahmad.codes@gmail.com>
2026-07-05 12:50:39 +0500

    Redirect guest area to login

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import { auth } from "../_lib/auth";
 
 export const metadata = {
@@ -6,7 +7,12 @@ export const metadata = {
 
 export default async function Page() {
   const session = await auth();
-  const firstName = session?.user?.name?.split(" ").at(0) || "guest";
+
+  if (!session?.user) {
+    redirect("/login");
+  }
+
+  const firstName = session.user.name?.split(" ").at(0) || "guest";
 
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
