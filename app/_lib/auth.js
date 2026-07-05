a2a403196a8f465dfdd6b84a5c514fb4fae31e62
naveedahmadcodes <naveedahmad.codes@gmail.com>
2026-07-05 12:25:43 +0500

    Initial commit

---
+++ app/_lib/auth.js
@@ -0,0 +1,52 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
+
+const authConfig = {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    authorized({ auth }) {
+      return true;
+    },
+    async signIn({ user }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest) {
+          await createGuest({ email: user.email, fullName: user.name });
+        }
+
+        return true;
+      } catch {
+        return true;
+      }
+    },
+    async session({ session }) {
+      try {
+        const guest = await getGuest(session.user.email);
+        if (guest) {
+          session.user.guestId = guest.id;
+        }
+      } catch {
+        session.user.guestId = null;
+      }
+
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
+
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth(authConfig);
